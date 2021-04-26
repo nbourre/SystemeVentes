917672066d8bb293fa1fe64041e34d1ed4d82087
Nicolas Bourré <nbourrereg@hotmail.com>
2021-04-26 16:50:27 -0400

    First test

---
+++ SystemeVentes.tests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+using System;
+using SystemeVentes.Models;
+using SystemeVentes.Services;
+using Xunit;
+
+namespace SystemeVentes.tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CalculTPS_ShouldBe_Valid()
+        {
+            /// Arrange
+            var product = new Product
+            {
+                SalePrice = 1.00M
+            };
+
+            var expected = 0.05M;
+            
+            /// Act
+            var actual = Calculatrice.CalculTPS(product);
+
+            /// Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
