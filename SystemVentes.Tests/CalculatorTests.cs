cd22f719c0240d610e0ad72ae37628684b67292e
nbourre <nbourrereg@hotmail.com>
2021-09-29 09:41:45 -0400

    Tests de fonctionnement avant le cours.

---
+++ SystemVentes.Tests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+using System;
+using SystemeVentes.Models;
+using SystemeVentes.Services;
+using Xunit;
+
+namespace SystemVentes.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CalculTPS_1_ShouldBe_0_05()
+        {
+            /// Arrange
+            var product = new Product
+            {
+                SalePrice = 1.00M
+            };
+
+            var expected = 0.05M;
+
+            /// Act
+            var actual = Calculatrice.CalculTPS(product);
+
+            /// Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1.00, 0.05)]
+        [InlineData(-10.00, -0.50)]
+        [InlineData(50.00, 2.50)]
+        [InlineData(1000.00, 50.00)]
+        public void CalculTPS_ShouldBe_Valid(decimal price, decimal expected)
+        {
+            /// Arrange
+            Product product = new() { SalePrice = price };
+
+
+            /// Act
+            var actual = Calculatrice.CalculTPS(product);
+
+            /// Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
